a7a3a3c710c37a3dfd223675669af259b1447c58
Zachary Pleasant <zpleasant10@gmail.com>
2018-03-06 17:41:38 -0600

    Added connection to the database and methods into orm file.

---
+++ config/orm.js
@@ -0,0 +1,55 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    // method that allows the user to select/show all rows in the database
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+    // method that allows user to insert new taco to the database
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+    // method that allows the user to update the eaten value in the database
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    },
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
