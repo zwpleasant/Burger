d8ae75340f810de4bb8739fb3a395f446221612e
Zachary Pleasant <zpleasant10@gmail.com>
2018-03-06 18:02:16 -0600

    Controller and model set up.

---
+++ models/taco.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var cat = {
+    selectAll: function(cb) {
+      orm.selectAll("tacos", function(res) {
+        cb(res);
+      });
+    },
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("tacos", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("tacos", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (tacos_controller.js).
+  module.exports = taco;
